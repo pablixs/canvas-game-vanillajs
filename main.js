9544eb0fa199a602d546260804191888c276a9fd
unknown <pablicjs@gmail.com>
2023-01-03 19:32:20 -0300

    Physics and textures

---
+++ main.js
@@ -0,0 +1,49 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+import { Background } from './background.js';
+
+window.addEventListener('load', () => {
+    
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.groundMargin = 50;
+            this.speed = 0;
+            this.maxSpeed = 3;
+            this.background = new Background(this);
+            this.player = new Player(this);
+            this.input = new InputHandler();
+        }
+
+        update(deltaTime){
+            this.background.update();
+            this.player.update(this.input.keys, deltaTime);
+        }
+
+        draw(context){
+            this.background.draw(context)
+            this.player.draw(context);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    console.log(game)
+    let lastTime = 0;
+
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        game.update(deltaTime);
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+
+    animate(0);
+})
